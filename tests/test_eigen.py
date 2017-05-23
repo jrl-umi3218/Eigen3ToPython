64512532cd774be6b2d425cd501a4bfa0c7e4956
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-05-23 18:27:59 +0900

    Add copyright notice

--- tests/test_eigen.py
+++ tests/test_eigen.py
@@ -1,3 +1,20 @@
+# Copyright 2012-2017 CNRS-UM LIRMM, CNRS-AIST JRL
+#
+# This file is part of Eigen3ToPython.
+#
+# Eigen3ToPython is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Eigen3ToPython is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Eigen3ToPython.  If not, see <http://www.gnu.org/licenses/>.
+
 import eigen as e
 import numpy as np
 from nose import with_setup
