1c4c3cb089c2539d85b2d18700fa1a97c7c0f6d0
Andrew Hundt <ATHundt@gmail.com>
2017-10-20 19:05:29 -0400

    test_eigen.py add extra multiplication test

--- tests/test_eigen.py
+++ tests/test_eigen.py
@@ -538,6 +538,7 @@ def test_quaternion():
   assert(q.norm() == 1.0)
   check_quaternion_almost_equals(e.Quaterniond.Identity().slerp(1.0, q), q)
   assert(q.squaredNorm() == 1.0)
+  q = q2*q_inv
 
 
 def test_angle_axis():
