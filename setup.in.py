f3572035428c2e5f023db82b08e98fa15cb8319c
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-11-12 20:29:14 +0100

    build(noble): Upgrade to pyproject.toml to replace deprecated use of pkg_resources

--- setup.in.py
+++ setup.in.py
@@ -4,77 +4,79 @@
 
 from __future__ import print_function
 
-
-import pkg_resources
-requirements_file = "@CMAKE_CURRENT_SOURCE_DIR@/requirements.txt"
-with open(requirements_file) as fd:
-  for pkg in fd:
-    pkg = pkg.strip()
-    pkg_resources.require(pkg)
+# Project metadata and dependencies are managed in pyproject.toml
 
 try:
-  from setuptools import setup
-  from setuptools import Extension
+    from setuptools import setup
+    from setuptools import Extension
 except ImportError:
-  from distutils.core import setup
-  from distutils.extension import Extension
+    from distutils.core import setup
+    from distutils.extension import Extension
 import sysconfig
 
 from Cython.Build import cythonize
 
 import numpy
 import os
-import subprocess
 import sys
 
 win32_build = os.name == 'nt'
 
 from utils import generate_eigen_pyx
 
-this_path  = os.path.dirname(os.path.realpath(__file__))
-src_files = ['eigen/c_eigen.pxd', 'eigen/c_eigen_private.pxd', 'include/eigen_wrapper.hpp']
-src_files.extend(['utils/angleaxis.in.pyx', 'utils/generate_eigen_pyx.py', 'utils/quaternion.in.pyx'])
-src_files = [ '{}/{}'.format(this_path, f) for f in src_files ]
+this_path = os.path.dirname(os.path.realpath(__file__))
+src_files = [
+    'eigen/c_eigen.pxd',
+    'eigen/c_eigen_private.pxd',
+    'include/eigen_wrapper.hpp',
+    'utils/angleaxis.in.pyx',
+    'utils/generate_eigen_pyx.py',
+    'utils/quaternion.in.pyx'
+]
+src_files = [f"{this_path}/{f}" for f in src_files]
 
-generate_eigen_pyx(this_path + "/eigen", this_path + "/utils")
+generate_eigen_pyx(f"{this_path}/eigen", f"{this_path}/utils")
 
 def GenExtension(name):
-  pyx_src = name.replace('.', '/')
-  cpp_src = pyx_src + '.cpp'
-  pyx_src = pyx_src + '.pyx'
-  ext_src = pyx_src
-  include_dirs = [os.path.join(os.getcwd(), "include"), "@EIGEN3_INCLUDE_DIR@", numpy.get_include()]
-  compile_args = ['-std=c++11']
-  if win32_build:
-      compile_args = ['-DWIN32']
-  elif sys.platform == 'darwin':
-    from platform import machine
-    osx_arch = machine()
-    os.environ["ARCHFLAGS"] = "-arch " + osx_arch
-    compile_args += ["-arch", osx_arch]
+    pyx_src = name.replace('.', '/') + '.pyx'
+    include_dirs = [os.path.join(os.getcwd(), "include"), "@EIGEN3_INCLUDE_DIR@", numpy.get_include()]
+    compile_args = ['-std=c++11']
+    if win32_build:
+        compile_args = ['-DWIN32']
+    elif sys.platform == 'darwin':
+        from platform import machine
+        osx_arch = machine()
+        os.environ["ARCHFLAGS"] = "-arch " + osx_arch
+        compile_args += ["-arch", osx_arch]
 
-  return Extension(name, [ext_src], extra_compile_args = compile_args, include_dirs = include_dirs)
+    return Extension(name, [pyx_src], extra_compile_args=compile_args, include_dirs=include_dirs)
 
 extensions = [
-  GenExtension('eigen.eigen')
+    GenExtension('eigen.eigen')
 ]
 
 packages = ['eigen']
 data = ['__init__.py', 'c_eigen.pxd', 'eigen.pxd']
 
-cython_c_compiler_launcher="@CYTHON_C_COMPILER_LAUNCHER@"
+cython_c_compiler_launcher = "@CYTHON_C_COMPILER_LAUNCHER@"
 if cython_c_compiler_launcher:
-  sysconfig.get_config_vars()["CC"] = cython_c_compiler_launcher + " " + sysconfig.get_config_vars()["BINDIR"] + "/" + sysconfig.get_config_vars()["CC"]
-cython_cxx_compiler_launcher="@CYTHON_CXX_COMPILER_LAUNCHER@"
+    sysconfig.get_config_vars()["CC"] = (
+        cython_c_compiler_launcher + " " +
+        sysconfig.get_config_vars()["BINDIR"] + "/" +
+        sysconfig.get_config_vars()["CC"]
+    )
+cython_cxx_compiler_launcher = "@CYTHON_CXX_COMPILER_LAUNCHER@"
 if cython_cxx_compiler_launcher:
-  sysconfig.get_config_vars()["CXX"] = cython_cxx_compiler_launcher + " " + sysconfig.get_config_vars()["BINDIR"] + "/" + sysconfig.get_config_vars()["CXX"]
+    sysconfig.get_config_vars()["CXX"] = (
+        cython_cxx_compiler_launcher + " " +
+        sysconfig.get_config_vars()["BINDIR"] + "/" +
+        sysconfig.get_config_vars()["CXX"]
+    )
 
-extensions = cythonize(extensions, cache = True)
+extensions = cythonize(extensions, cache=True)
 
 setup(
-    name = 'eigen',
-    version='@PROJECT_VERSION@',
-    ext_modules = extensions,
-    package_data = { 'eigen': data },
-    packages = packages
+    ext_modules=extensions,
+    package_data={'eigen': data},
+    packages=packages
 )
