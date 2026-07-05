a86f554fa67c5333aeaf3963848ef92db138c97b
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-07-05 17:05:58 +0200

    python: drop python2/user/system install, use venv

--- setup.in.py
+++ setup.in.py
@@ -4,8 +4,6 @@
 
 from __future__ import print_function
 
-# Project metadata and dependencies are managed in pyproject.toml
-
 try:
     from setuptools import setup
     from setuptools import Extension
@@ -13,16 +11,10 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 import sysconfig
-
-from Cython.Build import cythonize
-
-import numpy
 import os
 import sys
 
 win32_build = os.name == 'nt'
-
-from utils import generate_eigen_pyx
 
 this_path = os.path.dirname(os.path.realpath(__file__))
 src_files = [
@@ -35,9 +27,9 @@ src_files = [
 ]
 src_files = [f"{this_path}/{f}" for f in src_files]
 
-generate_eigen_pyx(f"{this_path}/eigen", f"{this_path}/utils")
-
 def GenExtension(name):
+    # Move numpy import here so it only triggers during actual compilation
+    import numpy 
     pyx_src = name.replace('.', '/') + '.pyx'
     include_dirs = [os.path.join(os.getcwd(), "include"), "@EIGEN3_INCLUDE_DIR@", numpy.get_include()]
     compile_args = ['-std=c++11']
@@ -51,32 +43,39 @@ def GenExtension(name):
 
     return Extension(name, [pyx_src], extra_compile_args=compile_args, include_dirs=include_dirs)
 
-extensions = [
-    GenExtension('eigen.eigen')
-]
+if __name__ == '__main__':
+    # Move Cython import and script generation inside the main block
+    from Cython.Build import cythonize
+    from utils import generate_eigen_pyx
 
-packages = ['eigen']
-data = ['__init__.py', 'c_eigen.pxd', 'eigen.pxd']
+    generate_eigen_pyx(f"{this_path}/eigen", f"{this_path}/utils")
 
-cython_c_compiler_launcher = "@CYTHON_C_COMPILER_LAUNCHER@"
-if cython_c_compiler_launcher:
-    sysconfig.get_config_vars()["CC"] = (
-        cython_c_compiler_launcher + " " +
-        sysconfig.get_config_vars()["BINDIR"] + "/" +
-        sysconfig.get_config_vars()["CC"]
+    extensions = [
+        GenExtension('eigen.eigen')
+    ]
+
+    packages = ['eigen']
+    data = ['__init__.py', 'c_eigen.pxd', 'eigen.pxd']
+
+    cython_c_compiler_launcher = "@CYTHON_C_COMPILER_LAUNCHER@"
+    if cython_c_compiler_launcher:
+        sysconfig.get_config_vars()["CC"] = (
+            cython_c_compiler_launcher + " " +
+            sysconfig.get_config_vars()["BINDIR"] + "/" +
+            sysconfig.get_config_vars()["CC"]
+        )
+    cython_cxx_compiler_launcher = "@CYTHON_CXX_COMPILER_LAUNCHER@"
+    if cython_cxx_compiler_launcher:
+        sysconfig.get_config_vars()["CXX"] = (
+            cython_cxx_compiler_launcher + " " +
+            sysconfig.get_config_vars()["BINDIR"] + "/" +
+            sysconfig.get_config_vars()["CXX"]
+        )
+
+    extensions = cythonize(extensions, cache=True)
+
+    setup(
+        ext_modules=extensions,
+        package_data={'eigen': data},
+        packages=packages
     )
-cython_cxx_compiler_launcher = "@CYTHON_CXX_COMPILER_LAUNCHER@"
-if cython_cxx_compiler_launcher:
-    sysconfig.get_config_vars()["CXX"] = (
-        cython_cxx_compiler_launcher + " " +
-        sysconfig.get_config_vars()["BINDIR"] + "/" +
-        sysconfig.get_config_vars()["CXX"]
-    )
-
-extensions = cythonize(extensions, cache=True)
-
-setup(
-    ext_modules=extensions,
-    package_data={'eigen': data},
-    packages=packages
-)
