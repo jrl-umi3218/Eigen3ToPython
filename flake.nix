0df6f16dd192825a210548933cf76d622df95a20
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-07-05 18:39:35 +0200

    python: adapt nix build

--- flake.nix
+++ flake.nix
@@ -8,9 +8,28 @@
     inputs.mc-rtc-nix.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        pyOverrideAttrs.eigen3-to-python = {
-          src = lib.cleanSource ./.;
-        };
+        pyOverrideAttrs.eigen3-to-python =
+          { pkgs-final, ... }:
+          {
+            src = lib.cleanSource ./.;
+
+            # Override the default CMake install step
+            installPhase = ''
+              runHook preInstall
+
+              # 1. Define the destination directory inside the Nix store output
+              # Using python.sitePackages handles the "lib/python3.13/site-packages" string automatically
+              local targetDir="$out/${pkgs-final.python3Packages.python.sitePackages}/eigen"
+              mkdir -p "$targetDir"
+
+              # 2. Copy your built files from the build tree to the target store path
+              # (Adjust 'build/python/Release/eigen/' path if your build folder structure differs slightly)
+              # TODO: changed here, use upstream
+              cp -r python3/Release/eigen/* "$targetDir"
+
+              runHook postInstall
+            '';
+          };
       }
     );
 }
